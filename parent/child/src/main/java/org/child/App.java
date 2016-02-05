8bcf7639c8b989501e8755d8c7eaeb17c7b66a53
Adious <adious.me@gmail.com>
2016-02-05 22:44:52 +0000

    child project created

--- parent/child/src/main/java/org/child/App.java
+++ parent/child/src/main/java/org/child/App.java
@@ -1,4 +1,4 @@
-package com.adious.parent;
+package org.child;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! from Parent class" );
+        System.out.println( "Hello World! from child" );
     }
 }
